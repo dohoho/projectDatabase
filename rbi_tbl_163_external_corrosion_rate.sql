540790f2ea1b4158bc83cb578dfffd1ea4eb642f
dohoho <dohoho.23.95@gmail.com>
2017-08-03 18:05:24 +0700

    all base 03-08

---
+++ rbi_tbl_163_external_corrosion_rate.sql
@@ -0,0 +1,54 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: rbi
+-- ------------------------------------------------------
+-- Server version	5.7.18-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tbl_163_external_corrosion_rate`
+--
+
+DROP TABLE IF EXISTS `tbl_163_external_corrosion_rate`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tbl_163_external_corrosion_rate` (
+  `OperatingTemp` int(11) NOT NULL,
+  `Marine` varchar(45) DEFAULT NULL,
+  `Temperate` varchar(45) DEFAULT NULL,
+  `Arid/Dry` varchar(45) DEFAULT NULL,
+  `Severe` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`OperatingTemp`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_163_external_corrosion_rate`
+--
+
+LOCK TABLES `tbl_163_external_corrosion_rate` WRITE;
+/*!40000 ALTER TABLE `tbl_163_external_corrosion_rate` DISABLE KEYS */;
+INSERT INTO `tbl_163_external_corrosion_rate` VALUES (10,'0','0','0','0'),(18,'1','0','0','3'),(43,'5','3','1','10'),(90,'5','3','1','10'),(160,'5','2','1','10'),(225,'1','0','0','2'),(250,'0','0','0','0');
+/*!40000 ALTER TABLE `tbl_163_external_corrosion_rate` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-08-03 18:03:27
